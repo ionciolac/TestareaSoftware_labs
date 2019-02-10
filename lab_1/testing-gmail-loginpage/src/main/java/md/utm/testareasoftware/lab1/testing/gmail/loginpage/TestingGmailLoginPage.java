a5c8d733fdb3a7ad9ce8256d223b8a28710b43fb
IonCiolac <wowgreep@gmail.com>
2019-02-10 22:36:12 +0200

    add functional for open chrom browser, open gmail page and imput login and password automatical and after login on gmail

--- lab_1/testing-gmail-loginpage/src/main/java/md/utm/testareasoftware/lab1/testing/gmail/loginpage/TestingGmailLoginPage.java
+++ lab_1/testing-gmail-loginpage/src/main/java/md/utm/testareasoftware/lab1/testing/gmail/loginpage/TestingGmailLoginPage.java
@@ -1,18 +1,32 @@
 package md.utm.testareasoftware.lab1.testing.gmail.loginpage;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class TestingGmailLoginPage {
     public static void main(String[] args) {
 
-        WebDriver browser = null;
+        WebDriver browser;
         System.setProperty("webdriver.chrome.driver", "/Applications/chromedriver");
-
         browser = new ChromeDriver();
 
-      
+        browser.manage().window().fullscreen();
+        browser.get("https://gmail.com");
+        browser.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        ((ChromeDriver) browser).findElementByName("identifier").sendKeys("testselenium1002@gmail.com");
+        ((ChromeDriver) browser).findElementById("identifierNext").click();
+        WebElement password = ((ChromeDriver) browser).findElementByName("password");
+        WebDriverWait wait = new WebDriverWait(browser,20);
+        wait.until(ExpectedConditions.elementToBeClickable(password));
+        password.sendKeys("utmti163");
+        ((ChromeDriver) browser).findElementById("passwordNext").click();
 
+        //browser.quit();
 
     }
 }
