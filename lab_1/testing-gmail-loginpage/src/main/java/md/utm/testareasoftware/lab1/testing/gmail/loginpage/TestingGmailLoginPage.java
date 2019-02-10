57402d899035fe32f1866ed25bd326b6f2c6b06c
IonCiolac <wowgreep@gmail.com>
2019-02-10 17:12:50 +0200

    add code to open Chrome

--- lab_1/testing-gmail-loginpage/src/main/java/md/utm/testareasoftware/lab1/testing/gmail/loginpage/TestingGmailLoginPage.java
+++ lab_1/testing-gmail-loginpage/src/main/java/md/utm/testareasoftware/lab1/testing/gmail/loginpage/TestingGmailLoginPage.java
@@ -1,7 +1,18 @@
 package md.utm.testareasoftware.lab1.testing.gmail.loginpage;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestingGmailLoginPage {
     public static void main(String[] args) {
+
+        WebDriver browser = null;
+        System.setProperty("webdriver.chrome.driver", "/Applications/chromedriver");
+
+        browser = new ChromeDriver();
+
+      
+
 
     }
 }
