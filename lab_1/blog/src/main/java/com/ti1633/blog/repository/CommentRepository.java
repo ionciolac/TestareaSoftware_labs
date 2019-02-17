61d528bb31b04e59f69b3ea0f16f43ffaef4d945
IonCiolac <wowgreep@gmail.com>
2019-02-17 19:00:57 +0200

    add testing-login-page project

--- lab_1/blog/src/main/java/com/ti1633/blog/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ti1633.blog.repository;
-
-import com.ti1633.blog.model.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
--- lab_1/blog/src/main/java/com/ti1633/blog/service/CommentService.java
+++
@@ -1,8 +0,0 @@
-package com.ti1633.blog.service;
-
-import com.ti1633.blog.model.Comment;
-
-public interface CommentService {
-
-    Comment save(Comment comment);
-}
